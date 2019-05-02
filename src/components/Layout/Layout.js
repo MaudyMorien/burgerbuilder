16c9e04dad25b09117337724099de25c7dbb388a
Maudy <maudymorien@hotmail.nl>
2019-05-02 15:15:22 +0200

    styling is dynamic

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Layout.css'
 import Toolbar from '../Navigation/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer'
 
 const layout = (props) => (
     <>
         <Toolbar />
+        <SideDrawer />
         <div>Toolbar, SideDrawer, Backdrop</div>
         <main className='Content'>
             {props.children}
