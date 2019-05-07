edb690c9c2d07e2e3bb00df11dad71a4dd2ce9b7
Maudy <maudymorien@hotmail.nl>
2019-05-07 18:10:45 +0200

    max 10 items comment

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -13,33 +13,10 @@ const Burger = props => {
     <div className="Burger">
       <BurgerIngredient type="bread-top" />
         {props.cart.length !== 0 && <RenderBurgerIngredients {...props} />}
-        {props.cart.length === 0 && <p>Make your ideal burger!</p>}
+        {props.cart.length === 0 && <p>Make your ideal burger with a maximum of 10 ingredients!</p>}
       <BurgerIngredient type="bread-bottom" />
     </div>
-  );
-};
-
-// See the difference
-// const burger = (props) => {
-//     let transformedIngredients = Object.keys(props.ingredients)
-//         .map(ingredient => [...Array(props.ingredients[ingredient])]
-//             .map((_, index) => <BurgerIngredient key={ingredient + index} type={ingredient} />
-//             )
-//         )
-//         .reduce((array, element) => {
-//             return array.concat(element)
-//         }, [])
-//     if (transformedIngredients.length === 0) {
-//         transformedIngredients = <p>Make your ideal burger!</p>
-//     }
-
-//     return (
-//         <div className='Burger'>
-//             <BurgerIngredient type='bread-top' />
-//             {transformedIngredients}
-//             <BurgerIngredient type='bread-bottom' />
-//         </div>
-//     )
-// }
+  )
+}
 
 export default Burger
