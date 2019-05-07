8e3e8bced81a72e643b6a8df2c7aecb0275a0564
Maudy <maudymorien@hotmail.nl>
2019-05-07 17:45:05 +0200

    cleanup plus colorchange

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -19,27 +19,4 @@ const Burger = props => {
   );
 };
 
-// See the difference
-// const burger = (props) => {
-//     let transformedIngredients = Object.keys(props.ingredients)
-//         .map(ingredient => [...Array(props.ingredients[ingredient])]
-//             .map((_, index) => <BurgerIngredient key={ingredient + index} type={ingredient} />
-//             )
-//         )
-//         .reduce((array, element) => {
-//             return array.concat(element)
-//         }, [])
-//     if (transformedIngredients.length === 0) {
-//         transformedIngredients = <p>Make your ideal burger!</p>
-//     }
-
-//     return (
-//         <div className='Burger'>
-//             <BurgerIngredient type='bread-top' />
-//             {transformedIngredients}
-//             <BurgerIngredient type='bread-bottom' />
-//         </div>
-//     )
-// }
-
 export default Burger
