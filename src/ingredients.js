06644a915cb0e4082cad6f4f2f408337be858914
Maudy <maudymorien@hotmail.nl>
2019-05-09 12:40:20 +0200

    added extra ingredients

--- src/ingredients.js
+++ src/ingredients.js
@@ -3,4 +3,8 @@ export default [
   { id: 2, name: 'Cheese', type: 'cheese', price: 0.4 },
   { id: 3, name: 'Meat', type:'meat', price: 1.3 },
   { id: 4, name: 'Bacon', type:'bacon', price: 0.7 },
+  { id: 5, name: 'Pineapple', type:'pineapple', price: 0.5 },
+  { id: 6, name: 'Tomato', type:'tomato', price: 0.5 },
+  { id: 7, name: 'Onion', type:'onion', price: 0.5 },
+
 ]
